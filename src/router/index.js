6d524d4aacb86e536600fbae3fec86d829a44ca1
khair-anna <aniakhair25@gmail..com>
2023-11-06 20:53:33 +0200

    router

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import LogIn from '@/views/LogIn.vue'
 import SignUp from '@/views/SignUp.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
